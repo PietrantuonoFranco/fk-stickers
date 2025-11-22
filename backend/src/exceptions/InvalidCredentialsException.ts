25d08b43ea85ab174e33b362251be2591ff98e8a
Franquinios <francopietrantuono999@gmail.com>
2025-11-22 00:09:27 -0300

    feat: update InvalidCredentialsException constructor parameter order for consistency

--- backend/src/exceptions/InvalidCredentialsException.ts
+++ backend/src/exceptions/InvalidCredentialsException.ts
@@ -1,7 +1,7 @@
 import Exception from "./Exception";
 
 export default class InvalidCredentialsException extends Exception{
-    constructor(message: string = "Unauthorized", detail: string) {
+    constructor(detail: string, message: string = "Unauthorized") {
         super(401, message, detail);
     }
 }
